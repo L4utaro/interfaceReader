359ca7f85e9ba879af377f4c98cb2bc6cc58ae66
Lautaro <lautarodebenedetti@gmail.com>
2019-04-26 16:00:22 -0300

    interface

---
+++ src/main/java/lectors/interfaces/ILector.java
@@ -0,0 +1,12 @@
+package lectors.interfaces;
+
+import java.util.List;
+
+public interface ILector {
+
+	public List<Object> getListOfJson(String keyValue);
+	
+	public void isAValidRoute();
+
+	public List<String> getNamesOfArrays();
+}
